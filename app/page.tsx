0342e9ec3cda09fad99f2c00325868dbbbb15128
Akhmad Qasim <akhmadqasim@gmail.com>
2025-03-11 13:05:15 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Akhmad Qasim - 2211102441237
+      </h1>
+      <p className="mb-4">
+
+      </p>
+    </section>
+  )
+}
